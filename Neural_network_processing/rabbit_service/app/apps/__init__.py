c7c6294d59d73f89c4b9f69c143fe06810606f09
onomylock <onomylock@outlook.com>
2023-09-29 10:50:11 +0700

    fix: fix service rabbit

--- Neural_network_processing/rabbit_service/app/apps/__init__.py
+++ Neural_network_processing/rabbit_service/app/apps/__init__.py
@@ -1,1 +1,2 @@
-from apps.handlers import router
+# from apps.handlers import router
+from apps.message_runner import gen_responce, run_neurals
--- Neural_network_processing/rabbit_service/app/serve.py
+++ Neural_network_processing/rabbit_service/app/serve.py
@@ -8,18 +8,18 @@ import anyio
 from propan.brokers.rabbit import RabbitExchange, ExchangeType
 from propan import PropanApp, RabbitBroker, RabbitRouter
 from propan.annotations import RabbitBroker as Broker, ContextRepo
-from app.apps.message_runner import gen_responce, run_neurals
+from apps.message_runner import gen_responce, run_neurals
 
 from config import init_settings
-from apps import router
+# from apps import router
 
-
+router = RabbitRouter()
 broker = RabbitBroker()
 broker.include_router(router)
 
 app = PropanApp(broker)
 
-router = RabbitRouter()
+
 exchInput = RabbitExchange(
     "StableDraw.Contracts.NeuralContracts.Requests:INeuralRequest", durable=True, type=ExchangeType.FANOUT)
 
@@ -27,16 +27,17 @@ exchOutput = RabbitExchange(
     "StableDraw.Contracts.NeuralContracts.Replies:INeuralReply", durable=True, type=ExchangeType.FANOUT)
 
 
-@router.handle(queue="neural-run", exchange=exchInput)
+@broker.handle(queue="generate-neural", exchange=exchInput)
 async def base_handler(body, logger: Logger):
 
-    body_json = json.loads(body.decode('utf8'))
-    msg = body_json['message']
-
+    body_json = json.loads(body.decode('utf8'))    
+    msg = body_json['message']    
+    
     response = run_neurals(msg)
     response = gen_responce(
-        body_json, response, "StableDraw.Contracts.NeuralContracts.Replies:INeuralReply")
-    await broker.publish(json.dumps(response), exchange=exchOutput)
+        body_json, response, "StableDraw.Contracts.NeuralContracts.Replies:INeuralReply")    
+    await broker.publish(response.encode('utf-8'), queue="neural-state", exchange=exchOutput)            
+    
 
 
 @app.on_startup
